c226c668a4b3725750797e78f1870a045258f49f
Priyobrato.Das <priyodas12@gmail.com>
2023-02-05 19:28:47 +0530

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -22,6 +22,10 @@ const routes: Routes = [
     path: 'user',
     component: UserComponent,
   },
+  {
+    path: 'user/:id',
+    component: UserComponent,
+  },
 ];
 
 @NgModule({
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
+  styleUrls: ['./user.component.css'],
 })
-export class UserComponent {
+export class UserComponent implements OnInit {
+  userId: any;
 
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.userId = this.route.snapshot.paramMap.get('id');
+    console.log(this.userId);
+  }
 }
